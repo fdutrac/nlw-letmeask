4f563e7c11420f634cb462af420e48abe487cddb
Fábio Dutra <fdutra.c@gmail.com>
2021-06-23 03:15:57 -0300

    Cria react-app

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import { Container } from './styles';
+type ButtonProps {
+    children?: string;
+}
+
+export const Button = (props: ButtonProps) => {
+    return (
+        <button>
+            Botão!
+        </button>
+    )
+}
